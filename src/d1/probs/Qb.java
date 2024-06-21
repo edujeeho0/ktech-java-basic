194750a5e0c1f2d2cd4e9762d085d96ebc3d13be
edujeeho <edujeeho@gmail.com>
2024-06-21 09:22:09 +0900

    qb

---
+++ src/d1/probs/Qb.java
@@ -0,0 +1,17 @@
+package d1.probs;
+
+import java.util.Scanner;
+
+// 사용자에게 3개의 정수를 입력받고,
+// 입력받은 순서의 반대로 출력하는 코드를 작성하시오.
+public class Qb {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(a);
+    }
+}
