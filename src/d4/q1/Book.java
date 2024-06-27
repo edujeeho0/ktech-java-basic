fdb6ac7c7ddbc581982af7cd76207984702d0c5c
edujeeho <edujeeho@gmail.com>
2024-06-27 10:46:03 +0900

    Book

--- src/d4/q1/Book.java
+++ src/d4/q1/Book.java
@@ -1,7 +1,69 @@
 package d4.q1;
 
-public class Book {
-    public Book() {
 
+public class Book {
+    public static int count;
+    private String title;
+    private String author;
+    private String category;
+    private int year;
+
+    // 생성자
+    // 객체를 만들때 실행되는
+    // 객체를 생성해주는 메서드
+    public Book() {}
+    public Book(
+            String title,
+            String author
+    ) {}
+
+    public Book(
+            String title,
+            String author,
+            int year
+    ) {
+        count++;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    // 무분별한 수정을 막고
+    // 정해진 방법으로만 데이터를 변경 가능하게 한다.
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        if (year > 2024) {
+            System.out.println("error");
+        }
+        else this.year = year;
+    }
+
+    // 반환(결과)하는 데이터가 없을때
+    public void printInfo() {
+        System.out.println(String.format(
+                "%s by %s, %d",
+                title,
+                author,
+                year
+        ));
     }
 }
--- src/d4/q1/Main.java
+++ src/d4/q1/Main.java
@@ -1,7 +1,55 @@
 package d4.q1;
 
+import java.util.Scanner;
+
+/*
+Book 클래스를 만들어보자.
+
+1. Book은 제목, 작가, 분류, 출판년도에 대한 정보를 가졌다.
+2. 생성자와 Getter, Setter 메서드는 자유롭게 만든다.
+3. `printInfo()` 메서드가 존재하며, 호출될 시 아래와 같은
+    출력이 일어난다.
+`<제목> by <작가>, <출판년도>`
+ */
 public class Main {
     public static void main(String[] args) {
-        Book book = new Book();
+//        Scanner scanner = new Scanner(System.in);
+//        // 결과는 어떤 형태의 데이터가 나오나?
+//        scanner.nextLong();
+
+        Book book = new Book(
+                "제목",
+                "미상",
+                2000
+        );
+        Book book2 = new Book(
+                "제목",
+                "미상",
+                2000
+        );
+        Book book3 = new Book(
+                "제목",
+                "미상",
+                2000
+        );
+        System.out.println(Book.count);
+//        book.title = "오펜하이머";
+//        book.setTitle("오펜하이머");
+//        book.setYear(2023);
+//        book.author = "미상";
+//        book.category = "문학";
+//        book.year = 2000;
+
+//        System.out.println(book.title);
+//        System.out.println(book.author);
+//        System.out.println(String.format(
+//                "%s by %s, %d",
+//                book.title,
+//                book.author,
+//                book.year
+//        ));
+//        book.title = "아인슈타인";
+        book.printInfo();
     }
+
 }
