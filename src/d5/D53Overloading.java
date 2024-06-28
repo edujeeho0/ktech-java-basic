4c2308445f1e4f303569a042d94cc1534cabb431
edujeeho <edujeeho@gmail.com>
2024-06-28 17:24:39 +0900

    varargs

--- src/d5/D53Overloading.java
+++ src/d5/D53Overloading.java
@@ -17,9 +17,35 @@ public class D53Overloading {
         System.out.println("asdf");
         System.out.println(2.38274);
         System.out.println(new Person("jeeho"));
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(arraySum(numbers, 1, 5));
+        System.out.println(arraySum(numbers, 1));
+    }
+
+    // 메서드 오버로딩으로 기본값 만들기
+    // 정수 배열을 받고, 시작 위치, 끝 위치 사이의 정수를 합하는 메서드
+    public static int arraySum(int[] arr, int s, int e) {
+        int sum = 0;
+        for (int i = s; i < e; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    public static int arraySum(int[] arr, int s) {
+        return arraySum(arr, s, arr.length);
     }
 
     public static int add(int a, int b) {
+        return a + b;
+    }
+    // 메서드 시그니처(method signature)가 달라야 한다.
+    /*public static long add(int a, int b) {
+        return a + b;
+    }*/
+
+    public static long add(int a, long b) {
         return a + b;
     }
 
---
+++ src/d5/D54Varargs.java
@@ -0,0 +1,36 @@
+package d5;
+
+// Variable Length Arguments
+// 변하는 길이의 인자들
+public class D54Varargs {
+    // 매개변수에 ...을 붙이면
+    // 메서드 내부에서는 배열처럼 사용하고,
+    public static int varNumSum(int... numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+
+    // 메서드 오버로딩시 배열을 가지고 오버로딩은 불가
+    // public static int varNumSum(int[] numbers) {}
+
+    public static void main(String[] args) {
+        System.out.println(String.format(
+                "%d", 10
+        ));
+        System.out.println(String.format(
+                "%d %s", 12, "asdf"
+        ));
+        System.out.println(String.format(
+                "%d %d %d %d %d %d %d",
+                0, 1, 2, 3, 4, 5, 6
+        ));
+        // 메서드 밖에서는 별도의 인자로 전달이 가능하다.
+        System.out.println(varNumSum(1,2,3,4,5,6,7,8));
+        // 배열을 전달하는것도 가능하다.
+        int[] numbers = {1, 2, 3};
+        System.out.println(varNumSum(numbers));
+    }
+}
