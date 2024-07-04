39ef9bfb430bac280751eda2d62bdaed6a3426c4
edujeeho <edujeeho@gmail.com>
2024-07-04 14:13:28 +0900

    list

--- src/d8/D83Collections.java
+++ src/d8/D83Collections.java
@@ -30,6 +30,34 @@ public class D83Collections {
         System.out.println(intArrayList);
         printEachElement(intArrayList);
 
+        // List
+        // 데이터의 중복을 허용하며, 순서를 지키는 데이터 모음
+        List<String> skillList = new ArrayList<>();
+        skillList.add("Java");
+        skillList.add("Python");
+        skillList.add("Kotlin");
+        skillList.add("Java");
+        // [Java, Python, Kotlin, Java]
+        System.out.println(skillList);
+        // get(int index): 몇번째의 데이터를 사용하겠다. [] 대신 쓴다.
+        System.out.println(skillList.get(0));
+        System.out.println(skillList.get(2));
+        // System.out.println(skillList.get(4));  // 지금은 예외발생
+
+        // 메서드기 때문에, 결과 값만 돌려주니까...[]로 데이터를 할당하기는 어렵다.
+        // skillList.get(2) = "";  // 컴파일 안됨
+        // 특정 위치에 add 하기
+        skillList.add(1, "Markdown");
+        // 특정 위치의 데이터 바꾸기
+        skillList.set(2, "Git");
+        System.out.println(skillList);
+
+        System.out.println(skillList.contains("Java"));
+        System.out.println(skillList.contains("Python"));
+        System.out.println(skillList.indexOf("Git"));
+        skillList.remove("Kotlin");
+        skillList.remove("Java");
+        System.out.println(skillList);
     }
 
     public static void printEachElement(List<Integer> intList) {}
